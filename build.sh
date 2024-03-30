623b0bc3c569dfde0aa6109c1a47c06363226027
sinofseven <em.s.00001@gmail.com>
2024-03-31 01:28:08 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -4,16 +4,18 @@ set -xeuo pipefail
 
 usage() {
   cat <<'EOT'
-build.sh --name LAYER_DIR_NAME --arch [amd64|arm64] --runtime-version RUNTIME_VERSION
+build.sh --name LAYER_DIR_NAME --arch [amd64|arm64] --runtime-version RUNTIME_VERSION --module MODULE_NAME
 
   --name directory having pyproject.toml in layers (ex: layer/base)
   --arch CPU architecture
   --runtime-version use python runtime version (default: 3.12) (ex: 3.12, 3.10)
+  --module use module (ex: zstd, zstd==1.5.5.2)
 EOT
 }
 
 layer_name=""
 architecture=""
+module=""
 runtime_version="3.12"
 
 for args in "$@"; do
@@ -27,6 +29,9 @@ for args in "$@"; do
     '--runtime-version')
       runtime_version=$2
       ;;
+    '--module')
+      module=$2
+      ;;
     '--help')
       usage
       exit 0
@@ -35,15 +40,19 @@ for args in "$@"; do
   shift
 done
 
-if [[ -z "${layer_name}" || -z "${architecture}" || -z "${runtime_version}" ]]; then
+if [[ -z "${layer_name}" || -z "${architecture}" || -z "${runtime_version}" || -z "${module}" ]]; then
   usage
   exit 1
 fi
 
-cd "layers/$layer_name"
 
-docker image build -f ../../Dockerfile -t $layer_name --platform linux/$architecture --build-arg="RUNTIME_VERSION=${runtime_version}" .
-docker container run --name=$layer_name $layer_name
-docker container cp $layer_name:/tmp/layer/python .
+mkdir -p layers/${layer_name}
+
+docker container run \
+  --name $layer_name \
+  -w /tmp \
+  "public.ecr.aws/sam/build-python${runtime_version}:latest" \
+  "pip install ${module} -t /tmp/python"
+
+docker container cp $layer_name:/tmp/python layers/${layer_name}/
 docker container rm $layer_name
-docker image rm $layer_name
